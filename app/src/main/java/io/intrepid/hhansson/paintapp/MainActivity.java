3cf60d7397be1f31c8591330f38e73038b99e4c5
Hayley Hansson <hayleyhansson@Hayleys-MacBook-Pro.local>
2015-06-09 16:47:34 -0400

    changing to fragment instead of blank activity

--- app/src/main/java/io/intrepid/hhansson/paintapp/MainActivity.java
+++ app/src/main/java/io/intrepid/hhansson/paintapp/MainActivity.java
@@ -1,17 +1,17 @@
 package io.intrepid.hhansson.paintapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_main);
     }
 
     @Override
