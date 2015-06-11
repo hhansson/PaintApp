00c97d8be6ba6a0f80d4b4e3aba0b7c476c78494
Hayley Hansson <hayleyhansson@Hayleys-MacBook-Pro.local>
2015-06-11 08:47:32 -0400

    touch interface basic functionality

--- app/src/main/java/io/intrepid/hhansson/paintapp/PaintingView.java
+++ app/src/main/java/io/intrepid/hhansson/paintapp/PaintingView.java
@@ -1,5 +1,6 @@
 package io.intrepid.hhansson.paintapp;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.Fragment;
 import android.content.Context;
@@ -10,6 +11,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,41 +23,25 @@ import android.widget.Button;
 /**
  * Created by hayleyhansson on 6/9/15.
  */
-public class PaintingView extends Fragment {
+public class PaintingView extends View {
 
     private Paint paint;
     private Path path;
-    private int paintColor = 0xFF660000;
+    private int paintColor;
     private Paint canvasPaint;
     private DisplayMetrics display = new DisplayMetrics();
     private Bitmap bitmap = null;
-    //private Button undoButton, colorButton, sizeButton;
-    //private Button redButton, yellowButton, blueButton;
     private Canvas canvas;
 
-   /* @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        undoButton = (Button) getView().findViewById(R.id.undo_button);
-        colorButton = (Button) getView().findViewById(R.id.color_selection_button);
-        sizeButton = (Button) getView().findViewById(R.id.size_selection_button);
-        redButton = (Button) getView().findViewById(R.id.red_button);
-        yellowButton = (Button) getView().findViewById(R.id.yellow_button);
-        blueButton = (Button) getView().findViewById(R.id.blue_button);
-        //Set onClick for all buttons
-        undoButton.setOnClickListener(this);
-        colorButton.setOnClickListener(this);
-        sizeButton.setOnClickListener(this);
-        redButton.setOnClickListener(this);
-        yellowButton.setOnClickListener(this);
-        blueButton.setOnClickListener(this);
+    public PaintingView(Context context, AttributeSet attributeSet){
+        super(context, attributeSet);
+    }
 
-        Log.v ("Custom Log: ", "onCreateView in Fragment accessed");
-        return inflater.inflate(R.layout.fragment_main, container, false);
-    }*/
 
     public void setupDrawing () {
         path = new Path();
         paint = new Paint();
+        paintColor = 0xff000000;
         paint.setColor(paintColor);
         paint.setAntiAlias(true);
         paint.setStrokeWidth(20);
@@ -65,41 +51,7 @@ public class PaintingView extends Fragment {
 
         canvasPaint = new Paint(Paint.DITHER_FLAG);
     }
-
-    /*public void onClick(View view){
-        switch (view.getId()){
-            case (R.id.undo_button):
-                break;
-            case (R.id.color_selection_button): redButton.setVisibility(View.VISIBLE);
-                yellowButton.setVisibility(View.VISIBLE);
-                blueButton.setVisibility(View.VISIBLE);
-                break;
-            case (R.id.size_selection_button):
-                break;
-            case (R.id.red_button):
-                Log.w("Log: ", "Red button click");
-                //Disappear color choices
-                redButton.setVisibility(View.GONE);
-                yellowButton.setVisibility(View.GONE);
-                blueButton.setVisibility(View.GONE);
-                //CHANGE COLOR TO RED
-                break;
-            case (R.id.yellow_button):
-                //Disappear color choices
-                redButton.setVisibility(View.GONE);
-                yellowButton.setVisibility(View.GONE);
-                blueButton.setVisibility(View.GONE);
-                //CHANGE COLOR TO YELLOW
-                break;
-            case (R.id.blue_button):
-                //Disappear color choices
-                redButton.setVisibility(View.GONE);
-                yellowButton.setVisibility(View.GONE);
-                blueButton.setVisibility(View.GONE);
-                //CHANGE COLOR TO BLUE
-                break;
-        }
-    }*/
+    
 
     public boolean onTouchEvent(MotionEvent event) {
         float touchX = event.getX();
