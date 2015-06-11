9aca85cda76aee9b9dd7bba8d8c646e285a35efd
Hayley Hansson <hayleyhansson@Hayleys-MacBook-Pro.local>
2015-06-11 10:16:19 -0400

    added button functionality to MainActivity, deleted ButtonActivity

--- app/src/main/java/io/intrepid/hhansson/paintapp/ButtonActivity.java
+++
@@ -1,7 +0,0 @@
-package io.intrepid.hhansson.paintapp;
-
-/**
- * Created by hayleyhansson on 6/10/15.
- */
-public class ButtonActivity {
-}
--- app/src/main/java/io/intrepid/hhansson/paintapp/MainActivity.java
+++ app/src/main/java/io/intrepid/hhansson/paintapp/MainActivity.java
@@ -3,15 +3,25 @@ package io.intrepid.hhansson.paintapp;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.zip.Inflater;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
     private PaintingView paintingView;
-    private ButtonActivity buttonActivity;
+    private Button undoButton;
+    private Button colorButton;
+    private Button sizeButton;
+    private Button redButton;
+    private Button yellowButton;
+    private Button blueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.painting_view);
         paintingView = (PaintingView) findViewById(R.id.painting);
         paintingView.setupDrawing();
+
+        undoButton = (Button) findViewById(R.id.undo_button);
+        colorButton = (Button) findViewById(R.id.color_selection_button);
+        sizeButton = (Button) findViewById(R.id.size_selection_button);
+        redButton = (Button) findViewById(R.id.red_button);
+        yellowButton = (Button) findViewById(R.id.yellow_button);
+        blueButton = (Button) findViewById(R.id.blue_button);
+        //Set onClick for all buttons
+        undoButton.setOnClickListener(this);
+        colorButton.setOnClickListener(this);
+        sizeButton.setOnClickListener(this);
+        redButton.setOnClickListener(this);
+        yellowButton.setOnClickListener(this);
+        blueButton.setOnClickListener(this);
+
     }
 
     @Override
@@ -41,5 +66,40 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case (R.id.undo_button):
+                break;
+            case (R.id.color_selection_button):
+                redButton.setVisibility(View.VISIBLE);
+                yellowButton.setVisibility(View.VISIBLE);
+                blueButton.setVisibility(View.VISIBLE);
+                break;
+            case (R.id.size_selection_button):
+                break;
+            case (R.id.red_button):
+                redButton.setVisibility(View.GONE);
+                yellowButton.setVisibility(View.GONE);
+                blueButton.setVisibility(View.GONE);
+                paintingView.changePaintColor(0xffff0000);
+                break;
+            case (R.id.yellow_button):
+                //Disappear color choices
+                redButton.setVisibility(View.GONE);
+                yellowButton.setVisibility(View.GONE);
+                blueButton.setVisibility(View.GONE);
+                paintingView.changePaintColor(0xffffff00);
+                break;
+            case (R.id.blue_button):
+                //Disappear color choices
+                redButton.setVisibility(View.GONE);
+                yellowButton.setVisibility(View.GONE);
+                blueButton.setVisibility(View.GONE);
+                paintingView.changePaintColor(0xff0000ff);
+                break;
+        }
     }
 }
